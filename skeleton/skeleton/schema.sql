81825e347520dc072766a2e63c25c47f7ac0ff71
KKolvin <57777208+KKolvin@users.noreply.github.com>
2022-02-16 15:42:09 -0500

    add tables for entities
    created new tables

--- skeleton/skeleton/schema.sql
+++ skeleton/skeleton/schema.sql
@@ -3,21 +3,67 @@ USE photoshare;
 DROP TABLE IF EXISTS Pictures CASCADE;
 DROP TABLE IF EXISTS Users CASCADE;
 
-CREATE TABLE Users (
-    user_id int4  AUTO_INCREMENT,
+CREATE TABLE Registered_Users (
+    user_id int4 AUTO_INCREMENT PRIMARY KEY,
+    fst_name varchar(20),
+    lst_name varchar(20),
     email varchar(255) UNIQUE,
-    password varchar(255),
-  CONSTRAINT users_pk PRIMARY KEY (user_id)
+    date_of_birth date,
+    hometown varchar(255),
+    gender varchar(20),
+    passcode varchar(20),
+    contribution int
 );
 
-CREATE TABLE Pictures
+CREATE TABLE Anonymous_User (
+	a_user_id int4 PRIMARY KEY 
+);
+
+CREATE TABLE Albums (
+	album_id int4 AUTO_INCREMENT PRIMARY KEY,
+	user_id int4, 
+	creation_date DATE, 
+	FOREIGN KEY (user_id) REFERENCES Registered_User(user_id) 
+		ON DELETE CASCADE
+);
+
+CREATE TABLE Photos
 (
-  picture_id int4  AUTO_INCREMENT,
+  photo_id int4 AUTO_INCREMENT PRIMARY KEY,
   user_id int4,
-  imgdata longblob ,
+  album_id int4,
+  imgdata longblob,
   caption VARCHAR(255),
   INDEX upid_idx (user_id),
-  CONSTRAINT pictures_pk PRIMARY KEY (picture_id)
+  FOREIGN KEY (user_id) REFERENCES Registered_User(user_id) 
+	ON DELETE CASCADE, 
+  FOREIGN KEY (album_id) REFERENCES albums(album_id) 
+	ON DELETE CASCADE 
 );
-INSERT INTO Users (email, password) VALUES ('test@bu.edu', 'test');
-INSERT INTO Users (email, password) VALUES ('test1@bu.edu', 'test');
+
+CREATE TABLE Comments ( 
+	com_id int4 AUTO_INCREMENT PRIMARY KEY, 
+    user_id int4, 
+    a_user_id int4, 
+    photo_id int4, 
+    content VARCHAR(255),
+    com_date DATE, 
+    FOREIGN KEY (user_id) REFERENCES Registered_User(user_id) 
+		ON DELETE CASCADE, 
+	FOREIGN KEY (a_user_id) REFERENCES Anonymous_User(a_user_id) 
+		ON DELETE CASCADE, 
+	FOREIGN KEY (photo_id) REFERENCES Photo(photo_id) 
+		ON DELETE CASCADE, 
+	CONSTRAINT register_or_not CHECK (
+		(user_id IS NULL AND a_user_id IS NOT NULL) 
+        OR 
+        (user_id IS NOT NULL AND a_user_id IS NULL)
+        ), 
+	CHECK (user_id <> (SELECT user_id FROM Photo P WHERE photo_id = P.photo_id)) 
+	);
+
+CREATE TABLE Tags(tag_name VARCHAR(50) PRIMARY KEY);
+
+
+INSERT INTO Registered_Users (email, password) VALUES ('test@bu.edu', 'test');
+INSERT INTO Registered_Users (email, password) VALUES ('test1@bu.edu', 'test');
